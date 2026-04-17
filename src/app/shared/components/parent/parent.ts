12952c0e16f5054abdcb1cfbb65bf01de1b62240
kasaramnaresh422 <nareshkasaram@gmail.com>
2026-04-17 07:41:17 +0530

    new changes

---
+++ src/app/shared/components/parent/parent.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-parent',
+  imports: [],
+  templateUrl: './parent.html',
+  styleUrl: './parent.scss',
+})
+export class Parent {}
